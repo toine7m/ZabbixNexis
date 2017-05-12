fe9009ca5e2fe4a33cc8d24c0a46f343080bda12
Antoine Massinon <toine7m@gmail.com>
2017-05-12 11:31:38 +0200

    Create config.php

---
+++ Zabbix Client/config.php
@@ -0,0 +1,22 @@
+<?php
+
+// ID du client créé au préalable dans le Zabbix Supervisor (Nexis)
+$clientID=17;
+
+// Nom du client
+$clientName='Client1';
+
+// IP du serveur Zabbix (client)
+$ipClient='10.254.0.10';
+
+// Nom d'utilisateur
+$userZabbix='zabirepo';
+$passwordZabbix='nexis369*';
+
+// IP du serveur Zabbix (Nexis)
+$ipNexis = '10.254.0.123';
+
+// Delai d'actualisation des données
+$delay=300;
+
+?>
