fa63819316597f3b777d7b481ebbf3d150270330
Antoine Massinon <toine7m@gmail.com>
2017-05-30 15:43:56 +0200

    Create config.php

---
+++ Zabbix Supervisor/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// Delay of item actualization rate
+$delay=300;
+
+// IP of the Supervisor
+$ipSupervisor='10.254.0.123';
+
+// Username of the Supervisor
+$username='admin';
+
+// Password of the Supervisor
+$password='zabbix';
+
+// Database information
+$dbserver="127.0.0.1";
+$dbuser="root";
+$dbpassword="zabbix";
+$db="supervisor";
+
+?>
